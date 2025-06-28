6ffda76cd6b524d69b2ccf52218d921e864c558e
Thiago Leite Ribeiro Campos <thiagoleiteribeirocampos@gmail.com>
2025-06-28 19:57:47 -0300

    Primeiro commit do desafio02(evento)

---
+++ evento/evento/src/test/java/com/devsuperior/desafio02/evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio02.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
